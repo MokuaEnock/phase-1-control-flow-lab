ad2d1ac8fb7c254ffc5f8adce29c883899f65fb6
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-08-15 19:33:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(ride) {
   // Write your code here!
+  if (ride <= 400) {
+    return "This one is on me!";
+  } 
+  else if (ride > 2000 && ride <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } 
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  if (city === 'NYC') {
+      return 'Ok, sounds good.'
+  } else {
+      return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(n){
+switch(n){
+  case "generous":
+    return "Thank you so much.";
+  case "not as generous":
+    return "Thank you.";
+  default:
+    return "Bye."
+}
 }
